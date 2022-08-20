8f1876dda54cbc6b719a67d3f72d3bb6a3a05c24
Kyle Oliver <oliverke@mail.uc.edu>
2022-08-20 03:13:50 -0400

    Removes unneeded methods from chess.base.ChessPiece

--- chess/piece/base.py
+++ chess/piece/base.py
@@ -17,26 +17,12 @@ class ChessPiece:
     def __repr__(self):
         return f'{self.__class__.__name__}({self.color.__class__.__name__}.{self.color.name})'
 
-    def __int__(self):
-        return self.value
-
     def __eq__(self, other):
         return self.__class__ == other.__class__ and self.color == other.color
 
     def __hash__(self):
         return hash(self.__class__) & hash(self.color)
 
-    def __gt__(self, other: SupportsInt):
-        return int(self) > int(other)
-
-    def __lt__(self, other: SupportsInt):
-        return int(self) < int(other)
-
-    def __ge__(self, other: SupportsInt):
-        return int(self) >= int(other)
-
-    def __le__(self, other: SupportsInt):
-        return int(self) <= int(other)
 
 
 
--- tests/test_piece/test_base.py
+++ tests/test_piece/test_base.py
@@ -15,9 +15,6 @@ def test_str():
 def test_repr():
     assert repr(piece.ChessPiece(Color.WHITE)) == 'ChessPiece(Color.WHITE)'
 
-def test_int():
-    assert int(piece.ChessPiece(Color.WHITE)) == 0
-
 def test_eq_with_same_class_and_same_color():
     assert piece.ChessPiece(Color.WHITE) == piece.ChessPiece(Color.WHITE)
 
@@ -41,55 +38,4 @@ def test_hash_with_same_value_different_color():
 
 def test_hash_with_different_value_different_color():
     assert hash(piece.ChessPiece(Color.WHITE)) != hash(piece.ChessPiece(Color.BLACK))
-
-def test_gt_with_greater():
-    assert (piece.ChessPiece(Color.WHITE) > -1) is True
-
-def test_gt_with_equal():
-    assert (piece.ChessPiece(Color.WHITE) > 0) is False
-
-def test_gt_with_less():
-    assert (piece.ChessPiece(Color.WHITE) > 1) is False
-
-def test_lt_with_less():
-    assert (piece.ChessPiece(Color.WHITE) < -1) is False
-
-def test_lt_with_equal():
-    assert (piece.ChessPiece(Color.WHITE) < 0) is False
-
-def test_lt_with_greater():
-    assert (piece.ChessPiece(Color.WHITE) < 1) is True
-
-def test_ge_with_greater():
-    assert (piece.ChessPiece(Color.WHITE) >= -1) is True
-
-def test_ge_with_equal():
-    assert (piece.ChessPiece(Color.WHITE) >= 0) is True
-
-def test_ge_with_less():
-    assert (piece.ChessPiece(Color.WHITE) >= 1) is False
-
-def test_le_with_less():
-    assert (piece.ChessPiece(Color.WHITE) <= -1) is False
-
-def test_le_with_equal():
-    assert (piece.ChessPiece(Color.WHITE) <= 0) is True
-
-def test_le_with_greater():
-    assert (piece.ChessPiece(Color.WHITE) <= 1) is True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
