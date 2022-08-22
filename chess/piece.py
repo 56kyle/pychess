f9ff2ebaf948b7f75831bf764bbfa8f91bcd40f1
Kyle Oliver <oliverke@mail.uc.edu>
2022-08-22 15:02:06 -0400

    Grouped up some piece info into child dataclasses

--- chess/piece.py
+++ chess/piece.py
@@ -6,25 +6,61 @@ from chess.color import Color
 @dataclass(frozen=True)
 class ChessPiece:
     color: Color
+    symbol: str
     name: str
     value: int
     letter: str
-    symbol: str
+
+@dataclass(frozen=True)
+class King(ChessPiece):
+    name: str = 'King'
+    value: int = 0
+    letter: str = 'K'
+
+@dataclass(frozen=True)
+class Queen(ChessPiece):
+    name: str = 'Queen'
+    value: int = 9
+    letter: str = 'Q'
+
+@dataclass(frozen=True)
+class Rook(ChessPiece):
+    name: str = 'Rook'
+    value: int = 5
+    letter: str = 'R'
+
+@dataclass(frozen=True)
+class Bishop(ChessPiece):
+    name: str = 'Bishop'
+    value: int = 3
+    letter: str = 'B'
+
+@dataclass(frozen=True)
+class Knight(ChessPiece):
+    name: str = 'Knight'
+    value: int = 3
+    letter: str = 'N'
+
+@dataclass(frozen=True)
+class Pawn(ChessPiece):
+    name: str = 'Pawn'
+    value: int = 1
+    letter: str = 'P'
 
 
-WhiteKing = ChessPiece(Color.WHITE, 'King', 0, 'K', '\u2654')
-WhiteQueen = ChessPiece(Color.WHITE, 'Queen', 9, 'Q', '\u2655')
-WhiteRook = ChessPiece(Color.WHITE, 'Rook', 5, 'R', '\u2656')
-WhiteBishop = ChessPiece(Color.WHITE, 'Bishop', 3, 'B', '\u2657')
-WhiteKnight = ChessPiece(Color.WHITE, 'Knight', 3, 'N', '\u2658')
-WhitePawn = ChessPiece(Color.WHITE, 'Pawn', 1, 'P', '\u2659')
+WhiteKing = King(Color.WHITE, '\u2654')
+WhiteQueen = Queen(Color.WHITE, '\u2655')
+WhiteRook = Rook(Color.WHITE, '\u2656')
+WhiteBishop = Bishop(Color.WHITE, '\u2657')
+WhiteKnight = Knight(Color.WHITE, '\u2658')
+WhitePawn = Pawn(Color.WHITE, '\u2659')
 
-BlackKing = ChessPiece(Color.BLACK, 'King', 0, 'K', '\u265A')
-BlackQueen = ChessPiece(Color.BLACK, 'Queen', 9, 'Q', '\u265B')
-BlackRook = ChessPiece(Color.BLACK, 'Rook', 5, 'R', '\u265C')
-BlackBishop = ChessPiece(Color.BLACK, 'Bishop', 3, 'B', '\u265D')
-BlackKnight = ChessPiece(Color.BLACK, 'Knight', 3, 'N', '\u265E')
-BlackPawn = ChessPiece(Color.BLACK, 'Pawn', 1, 'P', '\u265F')
+BlackKing = King(Color.BLACK, '\u265A')
+BlackQueen = Queen(Color.BLACK, '\u265B')
+BlackRook = Rook(Color.BLACK, '\u265C')
+BlackBishop = Bishop(Color.BLACK, '\u265D')
+BlackKnight = Knight(Color.BLACK, '\u265E')
+BlackPawn = Pawn(Color.BLACK, '\u265F')
 
 
 
