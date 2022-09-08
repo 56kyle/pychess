d3e88bb0dcd59e0935618c1a3c890a77f2b2c36c
Kyle Oliver <oliverke@mail.uc.edu>
2022-09-08 03:06:48 -0400

    Revert "Removes some misplaced methods from Game"
    This reverts commit 3b8454f0886fd9f32873c572acdf4e45cb6b64a1.

--- tests/test_game.py
+++ tests/test_game.py
@@ -16,6 +16,31 @@ def test_init(empty_game, empty_board):
     assert empty_game.turns == []
     assert empty_game.to_move == Color.WHITE
 
+def test__is_move_color(empty_game, e2_to_e4_move):
+    empty_game.to_move = Color.WHITE
+    assert empty_game._is_move_color(move=e2_to_e4_move)
+    empty_game.to_move = Color.BLACK
+    assert not empty_game._is_move_color(move=e2_to_e4_move)
+
+def test__is_capture_move_with_captured_unit(empty_game):
+    empty_game.board._array[7][0] = unit.WhiteRook()
+    empty_game.board._array[0][0] = unit.BlackRook()
+    move = Move(
+        unit=empty_game.board.get(Square(row=7, column=0)),
+        from_square=Square(row=7, column=0),
+        offset=UP*7,
+    )
+    assert empty_game._is_capture_move(move=move)
+
+def test__is_capture_move_with_no_captured_unit(empty_game):
+    empty_game.board._array[7][0] = unit.WhiteRook()
+    move = Move(
+        unit=empty_game.board.get(Square(row=7, column=0)),
+        from_square=Square(row=7, column=0),
+        offset=UP*7,
+    )
+    assert not empty_game._is_capture_move(move=move)
+
 def test_add_piece(empty_game):
     empty_game.add_piece(Square(row=0, column=0), unit.BlackRook())
     assert empty_game.board._array[0][0] == unit.BlackRook()
