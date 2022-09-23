cf8fb03330e88b4ae97064150d929a51e34cf527
Kyle Oliver <oliverke@mail.uc.edu>
2022-09-23 19:14:13 -0400

    Adds a bit to GameData

--- chess/game/data.py
+++ chess/game/data.py
@@ -1,14 +1,18 @@
 
 from abc import ABC
-from dataclasses import dataclass
-from typing import TypeVar
+from dataclasses import dataclass, field
+from typing import TypeVar, Set, Type
 
+from chess.board import BoardData
 from chess.data import AbstractData
+from chess.piece import Piece
 
 
 @dataclass(frozen=True)
 class GameData(AbstractData):
-    pass
+    board_data: BoardData
+    allowed_pieces: Set[Type[Piece]]
+    allowed_promotions: Set[Type[Piece]]
 
 
 T = TypeVar('T', bound=GameData)
--- chess/game/factory.py
+++ chess/game/factory.py
@@ -1,12 +1,27 @@
 from abc import ABC
-from typing import TypeVar
+from typing import TypeVar, Set, Type
 
+from chess.board import BoardData
 from chess.game.data import T, GameData
 from chess.factory import AbstractFactory
+from chess.piece import Piece
 
 
 class GameFactory(AbstractFactory[T], ABC):
     data_type: T = GameData
+
+    @classmethod
+    def create(cls,
+               board_data: BoardData,
+               allowed_pieces: Set[Type[Piece]],
+               allowed_promotions: Set[Type[Piece]],
+               *args,
+               **kwargs) -> T:
+        return cls.data_type(
+            board_data=board_data,
+            allowed_pieces=allowed_pieces,
+            allowed_promotions=allowed_promotions,
+        )
 
 
 F = TypeVar('F', bound=GameFactory)
