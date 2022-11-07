8ff5f82f0211b48eb579c03ca8d26b466fa6a907
Kyle Oliver <oliverke@mail.uc.edu>
2022-11-07 03:40:26 -0500

    Cleans up imports

--- chess/line.py
+++ chess/line.py
@@ -1,11 +1,7 @@
-import math
 from dataclasses import dataclass, replace
-from fractions import Fraction
-from typing import Set
 
 from chess.direction import Direction
 from chess.position import Position
-from chess.size import Size
 
 
 @dataclass(frozen=True)
